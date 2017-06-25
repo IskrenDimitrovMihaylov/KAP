a30914d66546d743e2be3adbf7a9c0d8b92a55cc
Iskren Mihaylov <iskren.mihajlov@gmail.com>
2017-06-25 13:02:55 +0000

    Created pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+#controlls all of the code for any page
+class PagesController < ApplicationController
+  
+  #back end code for pages/index
+  def index
+  end
+
+  #back end code for pages/home
+  def home
+  end
+
+  #back end code for pages/profile
+  def profile
+  end
+  
+  #back end code for pages/explore
+  def explore
+  end
+end
